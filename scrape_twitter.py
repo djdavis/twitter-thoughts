e904ec97b2c000c817da0a0d4ee0241b8836e9bc
Darius Davis <davis.darius889@yahoo.com>
2019-07-07 15:42:13 -0500

    initial commit

---
+++ scrape_twitter.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+import twint
+import datetime
+
+# Determine today's date
+today = datetime.date.today()
+# Format Twitter hashtag (based on current day).
+twitterHashTag = '#' + today.strftime("%A").lower() + 'thoughts'
+
+# Configure Twint to search for the above hashtag
+# We only want text-only results for the current date (limit 60).
+c = twint.Config()
+c.Search = twitterHashTag
+c.Limit = 60
+c.Store_json = True
+c.Custom["tweet"] = ["id", "date", "username", "created_at", "tweet"]
+c.User_full = True
+c.Output = "tweets.json"
+c.Format = "Tweet id: {id} | Date: {date} | Created: {created_at} | Tweet: {tweet} | dA"
+
+# Run
+twint.run.Search(c)
